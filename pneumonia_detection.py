45c01f4947b98e2ed72265ba8d748dff99f69b00
j-bd <recodata.out@gmail.com>
2019-07-14 19:15:06 +0200

    detection pipeline

--- pneumonia_detection.py
+++ pneumonia_detection.py
@@ -11,16 +11,16 @@ import time
 import cv2
 import os
 
-project_dir_path = "/home/latitude/Documents/Kaggle/rsna-pneumonia/yolo_v3/"
-image_path = project_dir_path + "test_data/obj/0a0f91dc-6015-4342-b809-d19610854a21.jpg"
 
 def detect(image_path, project_dir_path, confidence=0.5, threshold=0.0025):
     '''Detection of pneumonia on images'''
-    labels_path = project_dir_path + "data/obj.names"
-    weights_path = project_dir_path + "data/p_1400.weights"
-    config_path = project_dir_path + "data/yolo-obj.cfg"
+    labels_path = project_dir_path + "data/obj.names"#remove?
+    weights_path = project_dir_path + "test_data/p_1400.weights"
+    config_path = project_dir_path + "test_data/yolo-obj_test.cfg"
+    confidence=0.5
+    threshold=0.0025
 
-    labels = open(labels_path).read().strip().split("\n")
+    LABELS = open(labels_path).read().strip().split("\n")#remove?
 
     #Load YOLOv3 structure
     net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
@@ -33,14 +33,78 @@ def detect(image_path, project_dir_path, confidence=0.5, threshold=0.0025):
     ln = net.getLayerNames()
     ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-    # construct a blob from the input image and then perform a forward
-    # pass of the YOLO object detector, giving us our bounding boxes and
-    # associated probabilities
-    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    # Image preprocessing and Yolov3 detection
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=False, crop=False)
     net.setInput(blob)
-    start = time.time()
+    start = time.time()#remove?
     layerOutputs = net.forward(ln)
-    end = time.time()
+    end = time.time()#remove?
 
     # show timing information on YOLO
-    print(f"[INFO] Processing time : {end - start}seconds")
+    print(f"[INFO] Processing time : {end - start}seconds")#remove?
+
+    # initialize our lists of detected bounding boxes, confidences, and
+    # class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []#remove?
+
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            conf = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if conf > confidence:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(conf))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence, threshold)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+    	# loop over the indexes we are keeping
+    	for i in idxs.flatten():
+    		# extract the bounding box coordinates
+    		(x, y) = (boxes[i][0], boxes[i][1])
+    		(w, h) = (boxes[i][2], boxes[i][3])
+
+    		# draw a bounding box rectangle and label on the image
+    		color = (0,255,0)
+    		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+    		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+    		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+    			0.5, color, 2)
+
+    # show the output image
+    cv2.imshow("Image", image)
+
+
+project_dir_path = "/home/latitude/Documents/Kaggle/rsna-pneumonia/yolo_v3/"
+image_path = project_dir_path + "test_data/obj/0a8d486f-1aa6-4fcf-b7be-4bf04fc8628b.jpg"
+
+detect(image_path, project_dir_path, 0.5, 0.0025)
